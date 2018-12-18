20a5060d5bb33c70c825459cecc6af8e57d2f8c5
Ivan Mamontov <ivan_mamontov@homedepot.com>
2018-12-18 21:18:09 +0300

    Test version

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 import requests
 import numpy as np
@@ -7,28 +8,27 @@ import config
 import serial
 import time
 import EV3BT
-from timeit import default_timer as timer
 
 face_cascade = cv.CascadeClassifier(config.face_cascade)
 
-#response = requests.request("GET", config.api_url, headers=config.headers)
-#response.raise_for_status()
-#data = response.json()
 
-# assume sudo rfcomm connect hci0 00:16:53:56:43:DE
-# EV3 = serial.Serial('/dev/rfcomm0')
-# print("Listening for EV3 Bluetooth messages, press CTRL C to quit.")
-# left = 10
-# right = 10
-# print("Sending UP")
-# s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, 'up', 10)
-# EV3.write(s)
-# time.sleep(10)
-# print("Done. Sending DOWN")
-# s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, 'down', 10)
-# EV3.write(s)
-# print("Done!")
-# EV3.close()
+# response = requests.request("GET", config.api_url,  data=payload,  headers=config.headers)
+# response.raise_for_status()
+# data = response.json()
+
+def say_hello():
+    # assume sudo rfcomm connect hci0 00:16:53:56:43:DE
+    EV3 = serial.Serial('/dev/rfcomm0')
+    print("Sending UP")
+    s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, 'up', 10)
+    EV3.write(s)
+    time.sleep(15)
+    print("Done. Sending DOWN")
+    s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, 'down', 10)
+    EV3.write(s)
+    print("Done!")
+    EV3.close()
+
 
 for _ in range(100000):
     try:
@@ -36,16 +36,21 @@ for _ in range(100000):
         if resp.status_code == 200 and resp.headers['content-type'] == 'image/jpeg':
             img = np.asarray(bytearray(resp.content), dtype="uint8")
             img = cv.imdecode(img, cv.IMREAD_COLOR)
-            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            start = timer()
-            faces = face_cascade.detectMultiScale(gray, 1.05, 6)
-            end = timer()
-            print(end - start)  # Time in seconds, e.g. 5.38091952400282
-            if len(faces) > 0:
-                for (x, y, w, h) in faces:
-                    cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                cv.imwrite("{}.jpg".format(datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")), img)
-
-            print(len(faces))
+            height, width, channels = img.shape
+            if height == 1080 and width == 1920:
+                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray, 1.05, 6)
+                if len(faces) > 0:
+                    contains_big_face = False
+                    for (x, y, w, h) in faces:
+                        if w * h / 2073600 > 0.10:  # assume 1080 * 1920 = 2073600, so more than 10%
+                            cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                            contains_big_face = True
+                            print("size {}".format(w * h / 2073600))
+                    if contains_big_face:
+                        cv.imwrite(
+                            os.path.join("images", "{}.jpg".format(datetime.datetime.now().strftime("%d_%m_%Y_%H_%M"))),
+                            img)
+                        say_hello()
     except BaseException as error:
         print('An exception occurred in dispatch thread: {}'.format(error))
